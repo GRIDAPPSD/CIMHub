1105899a448c834f0ef474caab26de93f2676e53
temcdrm <Thomas.McDermott@pnnl.gov>
2020-06-08 15:52:16 -0700

    copy local opendsscmd build

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 mvn clean install
 
+# opendsscmd build product from fpc
+rm -rf distrib
+mkdir distrib
+cp ../OpenDSS/Source/CMD/test/opendsscmd distrib
+cp ../linenoise-ng/build/liblinenoise.so distrib
+
 ./stop.sh
 docker rmi temcderm/cimhub:0.0.1
 sudo docker build -t="temcderm/cimhub:0.0.1" .
