527353c56ebbf17f226792219d7bc4d6d54fe4eb
Shiva Poudel <shiva.poudel@pnnl.gov>
2021-04-22 12:34:01 -0700

    Measurements for OCHRE model

--- utils/insert_measurements.sh
+++ utils/insert_measurements.sh
@@ -1,6 +1,6 @@
-python3 InsertMeasurements.py cef2_der_lines_pq.txt
-python3 InsertMeasurements.py cef2_der_loads.txt
-python3 InsertMeasurements.py cef2_der_node_v.txt
-python3 InsertMeasurements.py cef2_der_special.txt
-python3 InsertMeasurements.py cef2_der_switch_i.txt
-python3 InsertMeasurements.py cef2_der_xfmr_pq.txt
+python3 utils/InsertMeasurements.py cimhubconfig.json ieee13ochre_lines_pq.txt
+python3 utils/InsertMeasurements.py cimhubconfig.json ieee13ochre_loads.txt
+python3 utils/InsertMeasurements.py cimhubconfig.json ieee13ochre_node_v.txt
+python3 utils/InsertMeasurements.py cimhubconfig.json ieee13ochre_special.txt
+python3 utils/InsertMeasurements.py cimhubconfig.json ieee13ochre_switch_i.txt
+python3 utils/InsertMeasurements.py cimhubconfig.json ieee13ochre_xfmr_pq.txt
