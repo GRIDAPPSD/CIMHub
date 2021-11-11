0488a89e2a2d5e628e2038596bdc0aab7b34f8fe
temcdrm <Thomas.McDermott@pnnl.gov>
2021-11-11 08:47:57 -0800

    first 6 cases organized for the test suite

--- der/clean.sh
+++ der/clean.sh
@@ -3,9 +3,9 @@ rm *.XML
 rm *.xml
 rm *.csv
 rm *.CSV
-# rm *.json
-rm -rf base
+rm base/*.csv
+rm base/convert_1547.dss
 rm -rf dss
 rm -rf glm
-rm convert_1547.dss
 rm upload_1547.sh
+rm checkglm.sh
