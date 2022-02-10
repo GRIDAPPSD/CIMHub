37e74631745e7f544d4ec7d22b16987f25717628
temcdrm <Thomas.McDermott@pnnl.gov>
2022-02-09 23:19:39 -0500

    builds a Python dictionary of query results

--- CPYDAR/ePHASORSIM.py
+++ CPYDAR/ePHASORSIM.py
@@ -1,4 +1,4 @@
-from SPARQLWrapper import SPARQLWrapper2
+from SPARQLWrapper import SPARQLWrapper2, JSON
 import cimhub.CIMHubConfig as CIMHubConfig
 import sys
 import xml.etree.ElementTree as ET
@@ -15,11 +15,26 @@ def initialize_sparql (cfg_file=None):
     CIMHubConfig.ConfigFromJsonFile (cfg_file)
 
   sparql = SPARQLWrapper2(CIMHubConfig.blazegraph_url)
+  sparql.setReturnFormat(JSON)
+#  print (sparql)
 
 def build_query (prefix, base, fid):
   idx = base.find('WHERE {') + 8
   retq = prefix + '\n' + base[:idx] + """ VALUES ?fdrid {{"_{:s}"}}\n""".format (fid) + base[idx:]
   return retq
+
+def build_dict (ret):
+  dict = {}
+  vars = ret.variables
+  keyfld = 'id'
+  vars.remove(keyfld)
+  for b in ret.bindings:
+    key = b[keyfld].value
+    row = {}
+    for fld in vars:
+      row[fld] = b[fld].value
+    dict[key] = row
+  return dict
 
 if __name__ == '__main__':
   cfg_file = 'cimhubconfig.json'
@@ -38,19 +53,19 @@ if __name__ == '__main__':
     queries[query.find('id').text.strip()] = query.find('value').text.strip()
 
   # test a query
-  print ('ListFeeders from XML file of queries')
-  sparql.setQuery (prefix + queries['DistFeeder'])
-  ret = sparql.query()
-  for b in ret.bindings:
-    print (' ', b['feeder'].value,b['fid'].value)
+# print ('ListFeeders from XML file of queries')
+# sparql.setQuery (prefix + queries['DistFeeder'])
+# ret = sparql.query()
+# for b in ret.bindings:
+#   print (' ', b['feeder'].value,b['fid'].value)
 
   fid = '4BE6DD69-8FE9-4C9F-AD44-B327D5623974'
   #  VALUES ?fdrid {"_4C4E3E2C-6332-4DCB-8425-26B628178374"}
-  print ('Photovoltaics')
+  print ('\nPhotovoltaics\n')
   query = build_query (prefix, queries['DistSolar'], fid)
-  print (query)
+#  print (query)
   sparql.setQuery (query)
   ret = sparql.query()
-  for b in ret.bindings:
-    print (' ', b['name'].value,b['bus'].value,b['ratedS'].value,b['ratedU'].value)
+  dict = build_dict (ret)
+  print (dict)
 
