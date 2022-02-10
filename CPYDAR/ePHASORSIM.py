792ce0062a56476e328ec221ac79e2401b80e97c
temcdrm <Thomas.McDermott@pnnl.gov>
2022-02-10 13:03:05 -0500

    most of the queries implemented for Python application code; still need support for compound keys

--- CPYDAR/ePHASORSIM.py
+++ CPYDAR/ePHASORSIM.py
@@ -1,9 +1,12 @@
 from SPARQLWrapper import SPARQLWrapper2, JSON
 import cimhub.CIMHubConfig as CIMHubConfig
 import sys
+import time
 import xml.etree.ElementTree as ET
+import pandas as pd
 
 sparql = None
+prefix = None
 
 # ieee13x  4BE6DD69-8FE9-4C9F-AD44-B327D5623974
 # ieee123x 4C4E3E2C-6332-4DCB-8425-26B628178374
@@ -16,7 +19,6 @@ def initialize_sparql (cfg_file=None):
 
   sparql = SPARQLWrapper2(CIMHubConfig.blazegraph_url)
   sparql.setReturnFormat(JSON)
-#  print (sparql)
 
 def build_query (prefix, base, fid):
   idx = base.find('WHERE {') + 8
@@ -36,36 +38,88 @@ def build_dict (ret):
     dict[key] = row
   return dict
 
+def query_for_values (tbl, fid):
+  keyfld = tbl['keyfld']
+  query = build_query (prefix, tbl['sparql'], fid)
+#  print (query)
+  sparql.setQuery (query)
+  ret = sparql.query()
+  vars = ret.variables
+  vars.remove (keyfld)
+  tbl['columns'] = vars
+  for b in ret.bindings:
+    key = b[keyfld].value
+    row = {}
+    for fld in vars:
+      if fld not in b:
+        row[fld] = ''
+      else:
+        if fld in ['name', 'phases', 'bus', 'state', 'conn', 'fdrid']:
+          row[fld] = b[fld].value
+        else:
+          try:
+            row[fld] = int(b[fld].value)
+          except ValueError:
+            try:
+              row[fld] = float(b[fld].value)
+            except ValueError:
+              row[fld] = b[fld].value
+
+    tbl['vals'][key] = row
+
+def summarize_dict (dict):
+  print ('Query ID                       Key Field   Nrows Columns')
+  for key, q in dict.items():
+    print ('{:30s} {:10s} {:6d} {:s}'.format(key, str(q['keyfld']), len(q['vals']), str(q['columns'])))
+
+def list_feeders (dict):
+  print ('Feeder Name          FID')
+  sparql.setQuery (prefix + dict['DistFeeder']['sparql'])
+  ret = sparql.query()
+  for b in ret.bindings:
+    print ('{:20s} {:s}'.format (b['feeder'].value, b['fid'].value))
+
+def list_table(tbl):
+  print ('key,{:s}'.format(str(tbl['columns'])))
+  for key, row in tbl['vals'].items():
+    print ('{:s},{:s}'.format (key, ','.join(str(row[c]) for c in tbl['columns'])))
+
+def load_feeder (dict, fid):
+  for key in ['DistSolar', 'DistStorage', 'DistLoad', 'DistCapacitor', 'DistLinesSpacingZ', 'DistSubstation', 'DistBaseVoltage', 'DistFeeder',
+              'DistBreaker', 'DistDisconnector', 'DistFuse', 'DistJumper', 'DistLoadBreakSwitch', 'DistRecloser', 'DistSectionaliser',
+              'DistOverheadWire', 'DistConcentricNeutralCable', 'DistLineSpacing',
+              'DistXfmrTank', 'DistXfmrBank', 'DistXfmrCodeRating', 'DistXfmrCodeNLTest', 'DistXfmrCodeSCTest',
+              'DistCoordinates', 'DistRegulatorBanked', 'DistRegulatorTanked',
+              'DistPowerXfmrCore', 'DistPowerXfmrMesh', 'DistSeriesCompensator',
+              'DistPhaseMatrix', 'DistSequenceMatrix', 'DistLinesCodeZ', 'DistLinesInstanceZ', 'DistTapeShieldCable']:
+    start_time = time.time()
+    query_for_values (dict[key], fid)
+    print ('Running {:30s} took {:6.3f} s'.format (key, time.time() - start_time))
+    list_table (dict[key])
+
 if __name__ == '__main__':
   cfg_file = 'cimhubconfig.json'
   if len(sys.argv) > 1:
     cfg_file = sys.argv[1]
   initialize_sparql (cfg_file)
 
-  # read the queries
+  # read the queries into dict
   tree = ET.parse('../q100.xml')
   root = tree.getroot()
   nsCIM = root.find('nsCIM').text.strip()
   nsRDF = root.find('nsRDF').text.strip()
   prefix = """PREFIX r: <{:s}>\nPREFIX c: <{:s}>""".format (nsRDF, nsCIM)
-  queries = {}
+  dict = {}
   for query in root.findall('query'):
-    queries[query.find('id').text.strip()] = query.find('value').text.strip()
-
-  # test a query
-# print ('ListFeeders from XML file of queries')
-# sparql.setQuery (prefix + queries['DistFeeder'])
-# ret = sparql.query()
-# for b in ret.bindings:
-#   print (' ', b['feeder'].value,b['fid'].value)
+    qid = query.find('id').text.strip()
+    dict[qid] = {}
+    dict[qid]['keyfld'] = query.find('keyfld').text
+    dict[qid]['sparql'] = query.find('value').text.strip()
+    dict[qid]['columns'] = []
+    dict[qid]['vals'] = {}
 
   fid = '4BE6DD69-8FE9-4C9F-AD44-B327D5623974'
-  #  VALUES ?fdrid {"_4C4E3E2C-6332-4DCB-8425-26B628178374"}
-  print ('\nPhotovoltaics\n')
-  query = build_query (prefix, queries['DistSolar'], fid)
-#  print (query)
-  sparql.setQuery (query)
-  ret = sparql.query()
-  dict = build_dict (ret)
-  print (dict)
+  list_feeders (dict)
+  load_feeder (dict, fid)
+  summarize_dict (dict)
 
