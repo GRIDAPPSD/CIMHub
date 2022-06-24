7dc5a05d6834ec3c0a409458cfb7df7b418a99e3
McDermott, Thomas E <thomas.mcdermott@pnnl.gov>
2022-06-24 12:47:57 -0400

    run from a release jar

--- tutorial/envars.bat
+++ tutorial/envars.bat
@@ -1,7 +1,7 @@
 echo off
 set DB_URL="http://localhost:9999/blazegraph/namespace/kb/sparql"
-set CIMHUB_PATH="../cimhub/target/cimhub-1.0.4-jar-with-dependencies.jar"
-rem set CIMHUB_PATH="../releases/cimhub-1.0.4.jar"
+rem set CIMHUB_PATH="../cimhub/target/cimhub-1.0.4-jar-with-dependencies.jar"
+set CIMHUB_PATH="../releases/cimhub-1.0.4.jar"
 rem set CIMHUB_PATH="../cimhub/target/libs/*;../cimhub/target/cimhub-1.0.1-SNAPSHOT.jar"
 set CIMHUB_PROG="gov.pnnl.gridappsd.cimhub.CIMImporter"
 
