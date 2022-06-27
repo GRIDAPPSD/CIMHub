221357f884315e4767848c299628638bbb3f3cd8
temcdrm <Thomas.McDermott@pnnl.gov>
2022-06-27 18:55:33 -0400

    cleaning up after tutorial on Linux

---
+++ tutorial/clean.sh
@@ -0,0 +1,15 @@
+rm *.xml
+rm ieee*.csv
+rm xf_*.csv
+rm weather.csv
+rm substation.csv
+rm pv_meters.csv
+rm prim_loads.csv
+rm feeder_head.csv
+rm summer.csv
+rm cim_test.dss
+rm go.sh
+rm aug11.hdf5
+rm -rf glm
+rm -rf dss
+
