bc907b50c1d75d5f9e73e92bc476534cfe61cc84
McDermott, Thomas E <thomas.mcdermott@pnnl.gov>
2022-06-28 13:23:01 -0400

    remove jsonschema to use Python 3.8 on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
-    'sphinx-jsonschema',
+#    'sphinx-jsonschema',
     'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
