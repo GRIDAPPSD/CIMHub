f7aa1ecd7ba0b2f17e2cec8463f8db2f660ed023
McDermott, Thomas E <thomas.mcdermott@pnnl.gov>
2022-06-29 01:12:25 -0400

    add housing data to the manifest

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
-rem rd /s /q dist
-rem python -m build
-rem twine check dist/* 
-rem twine upload -r testpypi dist/*
-twine upload dist/*
-rem pip install -i https://test.pypi.org/simple/ cimhub==1.0.5
+rd /s /q dist
+python -m build
+twine check dist/* 
+twine upload -r testpypi dist/*
+rem twine upload dist/*
+rem pip install -i https://test.pypi.org/simple/ cimhub==1.0.8
