89d64ee0019155d086f40d97e24e44b78ceb7da5
McDermott, Thomas E <thomas.mcdermott@pnnl.gov>
2022-11-20 17:43:25 -0500

    updating envars.* for distribution

--- queries/zipall.bat
+++ queries/zipall.bat
@@ -4,5 +4,5 @@ zip ..\releases\queries.zip cimhubjar.json
 zip ..\releases\queries.zip cimhubdocker.json
 zip ..\releases\queries.zip queries.txt
 zip ..\releases\queries.zip q100.xml
-
+zip ..\releases\queries.zip envars.*
 
