testing OpenDSS ECP; need line segment units, BESS dispmode and kwRating fixes

ecp/ecp_daily.py
@@ -1,11 +1,10 @@
 #  Copyright (c) 2022, Battelle Memorial Institute
 import os
-#import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import trapz
 
-plt.rcParams['savefig.directory'] = os.getcwd()
+plt.rcParams['savefig.directory'] = os.path.abspath ('..\docs\media') # os.getcwd()
 
 vbase = 7621.0
 tstep = 1.0
@@ -23,19 +22,21 @@ def make_timebase (d, base=1.0):
   n = d.shape[0]
   return np.linspace (0.0, tstep * float(n - 1) / base, n)
 
-if __name__ == '__main__':
-  d = np.loadtxt('./base/ecp_daily_Mon_pv1_1.csv', skiprows=1, delimiter=',')
+def add_case (ax, dsspath):
+  print ('Results from', dsspath)
+
+  d = np.loadtxt('./{:s}/ecp_daily_Mon_pv1_1.csv'.format(dsspath), skiprows=1, delimiter=',')
   t = make_timebase (d, base=tbase)
   pv1 = collect_columns (d, cols=[2,4,6])
-  d = np.loadtxt('./base/ecp_daily_Mon_pv2_1.csv', skiprows=1, delimiter=',')
+  d = np.loadtxt('./{:s}/ecp_daily_Mon_pv2_1.csv'.format(dsspath), skiprows=1, delimiter=',')
   pv2 = collect_columns (d, cols=[2,4,6])
-  d = np.loadtxt('./base/ecp_daily_Mon_gen1_1.csv', skiprows=1, delimiter=',')
+  d = np.loadtxt('./{:s}/ecp_daily_Mon_gen1_1.csv'.format(dsspath), skiprows=1, delimiter=',')
   gen1 = collect_columns (d, cols=[2,4,6])
-  d = np.loadtxt('./base/ecp_daily_Mon_gen2_1.csv', skiprows=1, delimiter=',')
+  d = np.loadtxt('./{:s}/ecp_daily_Mon_gen2_1.csv'.format(dsspath), skiprows=1, delimiter=',')
   gen2 = collect_columns (d, cols=[2,4,6])
-  d = np.loadtxt('./base/ecp_daily_Mon_load1_1.csv', skiprows=1, delimiter=',')
+  d = np.loadtxt('./{:s}/ecp_daily_Mon_load1_1.csv'.format(dsspath), skiprows=1, delimiter=',')
   load1 = collect_columns (d, cols=[2,4,6])
-  d = np.loadtxt('./base/ecp_daily_Mon_load2_1.csv', skiprows=1, delimiter=',')
+  d = np.loadtxt('./{:s}/ecp_daily_Mon_load2_1.csv'.format(dsspath), skiprows=1, delimiter=',')
   load2 = collect_columns (d, cols=[2,4,6])
 
   e1 = np.trapz (pv1, dx=tstep/tbase)
@@ -48,18 +49,24 @@ if __name__ == '__main__':
   e2 = np.trapz (load2, dx=tstep/tbase)
   print ('Total Energy Load1={:.2f} Load2={:.2f} kwh'.format (e1, e2))
 
+  ax[0].plot(t, pv1, label='PV 1 {:s}'.format(dsspath))
+  ax[0].plot(t, pv2, label='PV 2 {:s}'.format(dsspath))
+  ax[1].plot(t, gen1, label='Gen 1 {:s}'.format(dsspath))
+  ax[1].plot(t, gen2, label='Gen 2 {:s}'.format(dsspath))
+  ax[2].plot(t, load1, label='Load 1 {:s}'.format(dsspath))
+  ax[2].plot(t, load2, label='Load 2 {:s}'.format(dsspath))
+
+if __name__ == '__main__':
+
   fig, ax = plt.subplots(1, 3, figsize=(10,6))
+  plt.suptitle ('Case ecp_daily')
+
+  for dsspath in ['base', 'dssa']:
+    add_case (ax, dsspath)
+
   ax[0].set_ylabel('Solar Power [kW]')
-  ax[0].plot(t, pv1, color='red', label='PV 1 orig')
-  ax[0].plot(t, pv2, color='blue', label='PV 2 orig')
-
   ax[1].set_ylabel('Generator Power [kW]')
-  ax[1].plot(t, gen1, color='red', label='Gen 1 orig')
-  ax[1].plot(t, gen2, color='blue', label='Gen 2 orig')
-
   ax[2].set_ylabel('Load Power [kW]')
-  ax[2].plot(t, load1, color='red', label='Load 1 orig')
-  ax[2].plot(t, load2, color='blue', label='Load 2 orig')
 
   for i in range(3):
     ax[i].set_xlabel('Time [hr]')

ecp/ecp_duty.py
@@ -1,11 +1,10 @@
 #  Copyright (c) 2022, Battelle Memorial Institute
 import os
-#import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import trapz
 
-plt.rcParams['savefig.directory'] = os.getcwd()
+plt.rcParams['savefig.directory'] = os.path.abspath ('..\docs\media') # os.getcwd()
 
 vbase = 7621.0
 tstep = 1.0
@@ -23,11 +22,13 @@ def make_timebase (d, base=1.0):
   n = d.shape[0]
   return np.linspace (0.0, tstep * float(n - 1) / base, n)
 
-if __name__ == '__main__':
-  d1 = np.loadtxt('./base/ecp_duty_Mon_pv1_1.csv', skiprows=1, delimiter=',')
-  d2 = np.loadtxt('./base/ecp_duty_Mon_pv2_1.csv', skiprows=1, delimiter=',')
-  d3 = np.loadtxt('./base/ecp_duty_Mon_bess1_1.csv', skiprows=1, delimiter=',')
-  d4 = np.loadtxt('./base/ecp_duty_Mon_bess2_1.csv', skiprows=1, delimiter=',')
+def add_case (ax, dsspath):
+  print ('Results from', dsspath)
+
+  d1 = np.loadtxt('./{:s}/ecp_duty_Mon_pv1_1.csv'.format(dsspath), skiprows=1, delimiter=',')
+  d2 = np.loadtxt('./{:s}/ecp_duty_Mon_pv2_1.csv'.format(dsspath), skiprows=1, delimiter=',')
+  d3 = np.loadtxt('./{:s}/ecp_duty_Mon_bess1_1.csv'.format(dsspath), skiprows=1, delimiter=',')
+  d4 = np.loadtxt('./{:s}/ecp_duty_Mon_bess2_1.csv'.format(dsspath), skiprows=1, delimiter=',')
   pv1 = collect_columns (d1, cols=[2,4,6])
   pv2 = collect_columns (d2, cols=[2,4,6])
   bess1 = collect_columns (d3, cols=[2,4,6])
@@ -41,17 +42,25 @@ if __name__ == '__main__':
   b2 = np.trapz (bess2, dx=tstep/tbase)
   print ('Total Energy BESS1={:.2f} BESS2={:.2f} kwh'.format (b1, b2))
 
+  ax[0].plot(t, pv1, label='PV 1 {:s}'.format(dsspath))
+  ax[0].plot(t, pv2, label='PV 2 {:s}'.format(dsspath))
+  ax[1].plot(t, bess1, label='BESS 1 {:s}'.format(dsspath))
+  ax[1].plot(t, bess2, label='BESS 2 {:s}'.format(dsspath))
+
+if __name__ == '__main__':
+
   fig, ax = plt.subplots(1, 2, figsize=(10,6))
+  plt.suptitle ('Case ecp_duty')
+
+  for dsspath in ['base', 'dssa']:
+    add_case (ax, dsspath)
+
   ax[0].set_ylabel('Solar Power [kW]')
-  ax[0].plot(t, pv1, color='red', label='PV 1 orig')
-  ax[0].plot(t, pv2, color='blue', label='PV 2 orig')
   ax[0].set_xlabel('Time [hr]')
   ax[0].legend()
   ax[0].grid()
 
   ax[1].set_ylabel('Battery Power [kW]')
-  ax[1].plot(t, bess1, color='red', label='BESS 1 orig')
-  ax[1].plot(t, bess2, color='blue', label='BESS 2 orig')
   ax[1].set_xlabel('Time [hr]')
   ax[1].legend()
   ax[1].grid()

ecp/ecp_growthcvr.py
@@ -1,11 +1,10 @@
 #  Copyright (c) 2022, Battelle Memorial Institute
 import os
-#import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import trapz
 
-plt.rcParams['savefig.directory'] = os.getcwd()
+plt.rcParams['savefig.directory'] = os.path.abspath ('..\docs\media') # os.getcwd()
 
 vbase = 7621.0
 tstep = 3600.0
@@ -23,20 +22,27 @@ def make_timebase (d, base=1.0):
   n = d.shape[0]
   return np.linspace (0.0, tstep * float(n - 1) / base, n)
 
-if __name__ == '__main__':
-  fig, ax = plt.subplots(1, 1, figsize=(8,6))
-  ax.set_ylabel('Load Power [kW]')
+def add_case (ax, dsspath):
+  print ('Results from', dsspath)
   t = None
   for idx in range(5):
     key = 'load{:d}'.format(idx+1)
-    d = np.loadtxt('./base/ecp_growthcvr_Mon_{:s}_1.csv'.format(key), skiprows=1, delimiter=',')
+    d = np.loadtxt('./{:s}/ecp_growthcvr_Mon_{:s}_1.csv'.format(dsspath, key), skiprows=1, delimiter=',')
     if t is None:
       t = make_timebase (d, base=tbase)
     p = collect_columns (d, cols=[2,4,6])
     e = np.trapz (p, dx=tstep/tbase)
     print ('Total Energy {:s} = {:.2f} kwh'.format (key.upper(), e))
-    ax.plot(t, p, label=key.upper())
+    ax.plot(t, p, label='{:s} {:s}'.format(key.upper(), dsspath))
 
+if __name__ == '__main__':
+  fig, ax = plt.subplots(1, 1, figsize=(8,6))
+  plt.suptitle ('Case ecp_growthcvr')
+
+  for dsspath in ['base', 'dssa']:
+    add_case (ax, dsspath)
+
+  ax.set_ylabel('Load Power [kW]')
   ax.set_xlabel('Time [hr]')
   ax.legend()
   ax.grid()

ecp/ecp_harmonic.py
@@ -3,26 +3,33 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.rcParams['savefig.directory'] = os.getcwd()
+plt.rcParams['savefig.directory'] = os.path.abspath ('..\docs\media') # os.getcwd()
 
 classes = ['pv', 'load', 'gen', 'bess']
 
-if __name__ == '__main__':
-  fig, ax = plt.subplots(2, 4, figsize=(12,8))
-  plt.suptitle ('Harmonic Current Distortion')
+def add_case (ax, dsspath, offset):
+  print ('Results from', dsspath)
   t = None
   for col in range(4):
     for row in range(2):
       key = '{:s}{:d}'.format(classes[col], row+1)
-      d = np.loadtxt('./base/ecp_harmonic_Mon_{:s}_1.csv'.format(key), skiprows=1, delimiter=',')
+      d = np.loadtxt('./{:s}/ecp_harmonic_Mon_{:s}_1.csv'.format(dsspath, key), skiprows=1, delimiter=',')
       h = d[:,1]
       v = d[:,2] / d[0,2]
       i = d[:,3] / d[0,3]
       thdi = (np.sum(i**2) - 1.0) * 100.0
       print ('THDi {:6s} = {:6.2f} %'.format (key.upper(), thdi))
-      ax[row,col].bar(h, i, label=key.upper())
+      ax[row,col].bar(h + offset, i, width=0.45, label='{:s} {:s}'.format (key.upper(), dsspath))
       ax[row,col].set_ylabel('Magnitude [pu]')
       ax[row,col].set_xlabel('Harmonic')
       ax[row,col].legend()
+
+if __name__ == '__main__':
+  fig, ax = plt.subplots(2, 4, figsize=(12,8))
+  plt.suptitle ('Harmonic Current Distortion')
+  offset = -0.25
+  for dsspath in ['base', 'dssa']:
+    add_case (ax, dsspath, offset)
+    offset += 0.5
 
   plt.show()

ecp/ecp_temperature.py
@@ -1,11 +1,10 @@
 #  Copyright (c) 2022, Battelle Memorial Institute
 import os
-#import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import trapz
 
-plt.rcParams['savefig.directory'] = os.getcwd()
+plt.rcParams['savefig.directory'] = os.path.abspath ('..\docs\media') # os.getcwd()
 
 vbase = 7621.0
 tstep = 60.0
@@ -23,11 +22,12 @@ def make_timebase (d, base=1.0):
   n = d.shape[0]
   return np.linspace (0.0, tstep * float(n - 1) / base, n)
 
-if __name__ == '__main__':
-  d1 = np.loadtxt('./base/ecp_temperature_Mon_pv1_pq_1.csv', skiprows=1, delimiter=',')
-  d2 = np.loadtxt('./base/ecp_temperature_Mon_pv1_vi_1.csv', skiprows=1, delimiter=',')
-  d3 = np.loadtxt('./base/ecp_temperature_Mon_pv2_pq_1.csv', skiprows=1, delimiter=',')
-  d4 = np.loadtxt('./base/ecp_temperature_Mon_pv2_vi_1.csv', skiprows=1, delimiter=',')
+def add_case (ax, dsspath):
+  print ('Results from', dsspath)
+  d1 = np.loadtxt('./{:s}/ecp_temperature_Mon_pv1_pq_1.csv'.format(dsspath), skiprows=1, delimiter=',')
+  d2 = np.loadtxt('./{:s}/ecp_temperature_Mon_pv1_vi_1.csv'.format(dsspath), skiprows=1, delimiter=',')
+  d3 = np.loadtxt('./{:s}/ecp_temperature_Mon_pv2_pq_1.csv'.format(dsspath), skiprows=1, delimiter=',')
+  d4 = np.loadtxt('./{:s}/ecp_temperature_Mon_pv2_vi_1.csv'.format(dsspath), skiprows=1, delimiter=',')
   p1 = collect_columns (d1, cols=[2,4,6])
   v1 = collect_columns (d2, cols=[2,4,6], base=3.0*vbase)
   p2 = collect_columns (d3, cols=[2,4,6])
@@ -38,17 +38,23 @@ if __name__ == '__main__':
   e2 = np.trapz (p2, dx=tstep/tbase)
   print ('Total Energy PV1={:.2f} PV2={:.2f} kWh'.format (e1, e2))
 
+  ax[0].plot(t, p1, label='PV1 {:s}'.format(dsspath))
+  ax[0].plot(t, p2, label='PV2 {:s}'.format(dsspath))
+  ax[1].plot(t, v1, label='PV1 {:s}'.format(dsspath))
+  ax[1].plot(t, v2, label='PV2 {:s}'.format(dsspath))
+
+if __name__ == '__main__':
   fig, ax = plt.subplots(1, 2, figsize=(10,6))
+  plt.suptitle ('Case ecp_temperature')
+  for dsspath in ['base', 'dssa']:
+    add_case (ax, dsspath)
+
   ax[0].set_ylabel('Power [kW]')
-  ax[0].plot(t, p1, color='red', label='PV1 orig')
-  ax[0].plot(t, p2, color='blue', label='PV2 orig')
   ax[0].set_xlabel('Time [hr]')
   ax[0].legend()
   ax[0].grid()
 
   ax[1].set_ylabel('Voltage [pu]')
-  ax[1].plot(t, v1, color='red', label='PV1 orig')
-  ax[1].plot(t, v2, color='blue', label='PV2 orig')
   ax[1].set_xlabel('Time [hr]')
   ax[1].legend()
   ax[1].grid()