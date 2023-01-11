6e7e014aef4db2313e9ed95e1d9119e507c9d976
temcdrm <Thomas.McDermott@pnnl.gov>
2023-01-10 19:13:35 -0500

    new IEEE 9500 results for CIMHub 1.1.0

--- ieee9500/zipall.sh
+++ ieee9500/zipall.sh
@@ -8,7 +8,8 @@ zip ieee9500bal_compare.zip dss/ieee9500bal*.log
 zip ieee9500bal_compare.zip dss/ieee9500bal*.txt
 zip ieee9500bal_compare.zip glm/ieee9500bal*.csv
 zip ieee9500bal_compare.zip glm/ieee9500bal*.log
-zip ieee9500bal_compare.zip base/ieee9500bal*.csv
+zip ieee9500bal_compare.zip xml/ieee9500bal*.csv
+zip ieee9500bal_compare.zip onestep_bal.inc
 
 zip ieee9500unbal_csv.zip csv/ieee9500unbal*.csv
 zip ieee9500unbal_dss.zip dss/ieee9500unbal*.dss
@@ -18,6 +19,7 @@ zip ieee9500unbal_compare.zip dss/ieee9500unbal*.log
 zip ieee9500unbal_compare.zip dss/ieee9500unbal*.txt
 zip ieee9500unbal_compare.zip glm/ieee9500unbal*.csv
 zip ieee9500unbal_compare.zip glm/ieee9500unbal*.log
-zip ieee9500unbal_compare.zip base/ieee9500unbal*.csv
+zip ieee9500unbal_compare.zip xml/ieee9500unbal*.csv
+zip ieee9500unbal_compare.zip onestep_unbal.inc
 
 
